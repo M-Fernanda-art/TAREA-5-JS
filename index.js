6158b7cb49ff3dcc22b4a2094a4ad9d6fc1f8c32
M-Fernanda-art <fernanda201298@gmail.com>
2026-05-02 16:01:17 -0500

    promises done

--- index.js
+++ index.js
@@ -28,3 +28,54 @@ while (i < canciones.length) {
     console.log(canciones[i]);
     i++;
 }
+
+
+// PROMESAS
+
+// Es una operación futura que puede salir bien o mal, simula una decisión futura con éxito o error, 
+// sirven para esperar resultados, manejar procesos largos evitar bloqueos, organizar código asincronico.
+// Su funcionamiento es: pendiente, resuelto y rechaza o aprueba.
+
+// APROBAR O REPROBAR MATERIA
+
+const promesa = new Promise((resolve, reject) => {
+    let aprobado = true;
+
+    if (aprobado) {
+        resolve("Pasaste la materia");
+    } else {
+        reject("Reprobaste la materia");
+    }
+});
+
+promesa
+    .then(resultado => {
+        console.log(resultado);
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
+
+// DISPONIBILIDAD EN BIBLIOTECA
+
+
+const disponibilidad = new Promise((resolve, reject) => {
+    let disponible = true;
+
+    if (disponible) {
+        resolve("Puedes reservar el libro");
+    } else {
+        reject("El libro no esta disponible");
+    }
+});
+
+disponibilidad
+    .then(resultado => {
+        console.log(resultado);
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
+    
